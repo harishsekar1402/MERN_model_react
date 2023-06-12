ac444bb8953ac0d9982102628a6e145126d448e0
harishsekar1402 <srisaiharish1423@gmsil.com>
2023-06-12 15:31:00 +0530

    final

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,22 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Register.css'
 function Register() {
+  const [firstname, setfirstname] = useState('');
+  const [lastname, setlastname] = useState('');
+  const [email, setemail] = useState('');
+  const [branch, setbranch] = useState('');
+
+  const handlefirstnameChange = (event) => {
+    setfirstname(event.target.value);
+  };
+
+  const handlelastnameChange = (event) => {
+    setlastname(event.target.value);
+  };
+
+  const handleemailChange = (event) => {
+    setemail(event.target.value);
+  };
+  const handlebranchChange = (event) => {
+    setbranch(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('firstname:', firstname);
+    console.log('lastname:', lastname);
+    console.log('email :', email);
+    console.log('branch :',branch )
+  };
+
   return (
     <div className='register'>
-        <h1>World Bank Registration</h1><br></br><br></br>
-        <form>
-            <label>First name : </label>
-            <input type='text' placeholder='Enter the firstname' />
-            <label>Last name : </label>
-            <input type='text' placeholder='Enter the lastname' />
-            <label>email : </label>
-            <input type='email' placeholder='Enter the email' />
-            <label>Branch : </label>
-            <input type='text' placeholder='Enter the branch' />
-            <button type='submit'>Register</button>
-        </form>
+        <h1>World bank Registeration</h1>
+        <form onSubmit={handleSubmit}>
+        <label>Firstname:</label>
+        <input type="text" value={firstname} onChange={handlefirstnameChange}  placeholder='Enter the firstname'/>
+      
+        <label>lastname:</label>
+        <input type="text" value={lastname} onChange={handlelastnameChange}  placeholder='Enter the lastname'/>
+      
+        <label>email:</label>
+        <input type="email" value={email} onChange={handleemailChange}  placeholder='Enter the email'/>
+       
+        <label>Branch:</label>
+        <input type="text" value={branch} onChange={handlebranchChange} placeholder='Enter the branch'/>
+      
+      
+      <button type="submit">Register</button>
+    </form>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
